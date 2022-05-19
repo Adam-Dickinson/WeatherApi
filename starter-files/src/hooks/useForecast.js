91468bdfba512c9f0af1b36b0d9a4ebc7aa86f36
Adam Dickinson <adamgamesyt@gmail.com>
2022-05-19 21:30:18 +0200

    Update useForecast.js

--- starter-files/src/hooks/useForecast.js
+++ starter-files/src/hooks/useForecast.js
@@ -9,13 +9,39 @@ const useForecast = () =>{
     const [isError, setError] = useState(false)
     const [isLoading, setLoading] = useState(false)
     const [forecast, setForecast] = useState(null)
+
+    const getWoeid = async (location) => {
+        const {data} = await axios(`${REQUEST_URL}/serach`, {params: {query: location}});
+
+        console.log({data});
+    if(!data || data.length === 0) {
+            setError('There is no such location')
+            setLoading(false)
+            return;
+        }
+        return data
+    }
+
+    const getForecastData = async (woeid) => {
+        const {data} = await axios(`${REQUEST_URL}/${woeid}`)
+
+        if (!data || data.legth === 0) {
+            setError('Something Went Wrong')
+            setLoading(false)
+            return;
+        }        
+            return data[0];
+    }
+
  
 
     const submitRequest = async location => {
-        const response = await axios(`${REQUEST_URL}/serach`, {params: {query: location}});
-
-        console.log({response});
-    }
+        setLoading(true);
+        setError(false);
+        const response = await getWoeid(location);
+        if(!response?.woeid) return;
+        const data = await getForecastData(response.woeid)
+    };
 
 
     return {
