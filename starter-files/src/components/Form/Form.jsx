939aa603a620481d7c606ce3b96fd17712e82735
Adam Dickinson <adamgamesyt@gmail.com>
2022-05-19 20:19:49 +0200

    Update Form.jsx
    Saved Form.jsx

--- starter-files/src/components/Form/Form.jsx
+++ starter-files/src/components/Form/Form.jsx
@@ -4,9 +4,10 @@ import styles from './Form.module.css';
 import { useState } from 'react';
 
 const Form = () => {
-    
+    const [location, setlocation] = useState('')
     const onSubmit = e =>{
         e.preventDefault();
+        if (!location || location === '') return;
     }
     return (
         <form onSubmit={onSubmit}>
@@ -16,6 +17,8 @@ const Form = () => {
                 className={`${styles.input} form-control`}
                 placeholder="Search for location"
                 required
+                value={location}
+                onChange={e => setlocation(e.target.value)}
             />
 
             <button type="submit" className={styles.button} onClick={onSubmit}>
