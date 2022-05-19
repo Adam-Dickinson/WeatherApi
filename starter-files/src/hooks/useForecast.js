5627c7e59e2a230475cc6efa290ac3ae023c3bd5
Adam Dickinson <adamgamesyt@gmail.com>
2022-05-19 20:27:57 +0200

    Update useForecast.js
    Created const for Error, Loading and Forecast

--- starter-files/src/hooks/useForecast.js
+++ starter-files/src/hooks/useForecast.js
@@ -1,5 +1,13 @@
-const useForecast = () =>{
+import { useState } from "react";
 
+const useForecast = () =>{
+    const [isError, setError] = useState(false)
+    const [isLoading, setLoading] = useState(false)
+    const [forecast, setForecast] = useState(false)
+
+    return {
+        isError, isLoading, forecast
+    }
 }
 
 export default useForecast;
